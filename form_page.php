e8f8f60a96d164dc3ccc11980b0c1c03b3e8e52e
dannny3dee <85377491+dannny3dee@users.noreply.github.com>
2021-06-16 10:29:34 +0530

    Add files via upload
    completed the web page using basic css and html

---
+++ form_page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <p> welcome
+    Welcome <?php echo $_GET["fname"] ; ?><br>
+Your last name  is: <?php echo $_GET["lname"]; ?></p>
+    
+
+</body>
+</html>
